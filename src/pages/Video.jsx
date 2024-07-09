0a1b45d8273902943376716437cc5c1baea23130
TaePyeongKang <dramayeong@naver.com>
2024-07-09 11:18:10 +0900

    메인 섹션 나머지 콘텐츠 작업' ㅊㅊㅊ exit dd asdfasdfasfd

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import Main from '../components/section/Main'
+import { webdText } from '../data/webd'
+import { Link } from 'react-router-dom'
 
 const Video = () => {
     return (
         <Main 
             title = "유튜브 비디오 영상"
             description="유튜브 비디오 영상을 볼 수 있습니다.">
-            Video
+            <section id='videoPage'>
+            <h2>🤣 웹디자인기능사 준비는 이걸로!</h2>
+            <div className="video__inner">
+                {webdText.map((video, key) => (
+                    <div className="video" key={key}>
+                        <div className="video__thumb play__icon">
+                            <Link to={`/video/${video.videoId}`}>
+                                <img src={video.img} alt={video.title} />
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+                </div>
+            </section>
         </Main>
     )
 }
